726ef6657d90da85122c9219769b8c52d87c8a1a
Agathe <agathe.sauvestre@insa-lyon.fr>
2018-12-11 11:52:36 +0100

    nouvelles pages

--- app/src/main/java/com/example/joanc/insaide/Login.java
+++ app/src/main/java/com/example/joanc/insaide/Login.java
@@ -3,11 +3,11 @@ package com.example.joanc.insaide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 }
