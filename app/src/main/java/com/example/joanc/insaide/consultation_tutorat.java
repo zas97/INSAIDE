b2bcf126ce78abd3e87d4334ad6c27df636ba50d
Agathe <agathe.sauvestre@insa-lyon.fr>
2018-12-14 09:52:07 +0100

    fenetre consultation tutorat en mode pas encore inscrit

---
+++ app/src/main/java/com/example/joanc/insaide/consultation_tutorat.java
@@ -0,0 +1,13 @@
+package com.example.joanc.insaide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class consultation_tutorat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.consultation_tutorat);
+    }
+}
