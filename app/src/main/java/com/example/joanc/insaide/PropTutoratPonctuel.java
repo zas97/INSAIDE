fd07bbbb330ad3f66dec36cd9c96a46f951b9eff
jaxalo <ismail.jattioui1@gmail.com>
2019-01-08 04:24:38 +0100

    creer tutorat recurent ou ponctuels (les champs)

--- app/src/main/java/com/example/joanc/insaide/PropTutoratPonctuel.java
+++ app/src/main/java/com/example/joanc/insaide/PropTutoratPonctuel.java
@@ -7,10 +7,21 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class PropTutoratPonctuel extends AppCompatActivity {
+
+    TextView hide1;
+    EditText hide2;
+    TextView hide3;
+    Spinner hide4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,40 @@ public class PropTutoratPonctuel extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        RadioButton ponctuel = findViewById(R.id.ponctuelRadioBtn);
+        RadioButton recurent = findViewById(R.id.recurrentRadioBtn);
+
+        hide1 = findViewById(R.id.hide1);
+        hide2 = findViewById(R.id.hide2);
+        hide3 = findViewById(R.id.hide3);
+        hide4 = findViewById(R.id.hide4);
+
+
+        hide1.setVisibility(hide1.INVISIBLE);
+        hide2.setVisibility(hide2.INVISIBLE);
+
+        ponctuel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hide1.setVisibility(hide1.VISIBLE);
+                hide2.setVisibility(hide2.VISIBLE);
+                hide3.setVisibility(hide3.INVISIBLE);
+                hide4.setVisibility(hide4.INVISIBLE);
+            }
+        });
+
+        recurent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hide1.setVisibility(hide1.INVISIBLE);
+                hide2.setVisibility(hide2.INVISIBLE);
+                hide3.setVisibility(hide3.VISIBLE);
+                hide4.setVisibility(hide4.VISIBLE);
+            }
+        });
+
+
     }
 
     public boolean onContextItemSelected (MenuItem item) {
